a6038c4017e92fc9bc3b8a31f3a1d7ac19fbeb3a
César Ortiz <cesaraugustoortiz26@gmail.com>
2023-03-10 16:37:35 -0400

    funcrion para crear las alertas de las acciones creada

--- client/src/users/pages/Users.jsx
+++ client/src/users/pages/Users.jsx
@@ -1,47 +1,27 @@
 import React, { useState } from "react";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import { Formik, Form } from "formik";
 import Modal from "../../components/Modal";
 import { signup } from "../../firebase/auth";
 import { addWithId, USERS } from "../../firebase/firestore";
+import submitWithAlerts from "../../utils/submitWithAlerts";
 import UsersForm from "../components/UserForm";
 import UsersTable from "../components/UserTable";
 import { initialValues, validationSchema } from "../components/userValidationSchema";
-import * as Swal from "sweetalert2";
 
-const errorMessages = {
-  "auth/email-already-in-use": "El correo ya esta en uso",
-  "auth/invalid-email": "El correo no es valido",
-  "auth/weak-password": "La contraseña debe tener al menos 6 caracteres",
-  default: "Error al crear el usuario",
-};
-
-const loader = () =>
-  Swal.fire({
-    didOpen: () => Swal.showLoading(),
-    didClose: () => Swal.hideLoading(),
-    title: "Espere un momento",
-    text: "Estamos procesando su información",
-    allowOutsideClick: false,
-    allowEscapeKey: false,
-    allowEnterKey: false,
-    showConfirmButton: false,
-    icon: "info",
-  });
-
-const uploadAlert = () =>
-  Swal.fire({
+const config = {
+  confirm: {
     title: "¿Seguro que desea crear este usuario?",
-    icon: "warning",
-    showCancelButton: true,
     confirmButtonColor: "#992127",
     confirmButtonText: "Si, crear",
-    cancelButtonText: "Cancelar",
-  });
+  },
+  success: { title: "Usuario creado" },
+  error: { confirmButtonColor: "#992127" },
+};
 
 export function Users(props) {
   const [open, setOpen] = useState(false);
@@ -83,31 +63,12 @@ export function Users(props) {
         {/* TODO manejar todos los errores  */}
         <Formik
           onSubmit={async (values, { resetForm }) => {
-            uploadAlert()
-              .then(async (response) => {
-                if (!response.isConfirmed) return;
-                loader();
-                const user = await signup(values.email, values.password);
-                delete values.password;
-                await addWithId(USERS, user.uid, { ...values, uid: user.uid, active: true });
-                return response;
-              })
-              .then((response) => {
-                if (!response) return;
-                Swal.fire({
-                  title: "Usuario creado",
-                  icon: "success",
-                });
-              })
-              .catch((error) => {
-                Swal.fire({
-                  title: "Ha ocurrido un error",
-                  text: errorMessages[error.code] || errorMessages.default,
-                  icon: "error",
-                  confirmButtonColor: "#992127",
-                });
-              });
-
+            const submit = async () => {
+              const user = await signup(values.email, values.password);
+              delete values.password;
+              await addWithId(USERS, user.uid, { ...values, uid: user.uid, active: true });
+            };
+            await submitWithAlerts(submit, config);
             resetForm();
             setOpen(false);
           }}
---
+++ client/src/utils/submitWithAlerts.js
@@ -0,0 +1,89 @@
+import * as Swal from "sweetalert2";
+
+const errorMessages = {
+  "auth/email-already-in-use": "El correo ya esta en uso",
+  "auth/invalid-email": "El correo no es valido",
+  "auth/weak-password": "La contraseña debe tener al menos 6 caracteres",
+  default: "Error al crear el usuario",
+};
+
+const confirm = (config = {}) =>
+  Swal.fire({
+    title: "¿Seguro que desea realizar esta acción?",
+    icon: "warning",
+    showCancelButton: true,
+    // confirmButtonColor: "#992127",
+    confirmButtonText: "Si",
+    cancelButtonText: "Cancelar",
+    ...config,
+  });
+
+const loading = () =>
+  Swal.fire({
+    // didOpen: () => Swal.showLoading(),
+    // didClose: () => Swal.hideLoading(),
+    title: "Espere un momento",
+    text: "Estamos procesando su información",
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    showConfirmButton: false,
+    icon: "info",
+  });
+
+const onSuccess = (config = {}) =>
+  Swal.fire({
+    title: "Acción realizada con éxito",
+    icon: "success",
+    ...config,
+  });
+// setTimeout(() => {
+//   Swal.fire({
+//     title: "Acción realizada con éxito",
+//     icon: "success",
+//     ...config,
+//   });
+// }, 10);
+
+const onError = async (error, config = {}) =>
+  Swal.fire({
+    title: "Ha ocurrido un error",
+    text: errorMessages[error.code] || errorMessages.default,
+    icon: "error",
+    // confirmButtonColor: "#992127",
+    ...config,
+  });
+// setTimeout(() => {
+//   Swal.fire({
+//     title: "Ha ocurrido un error",
+//     text: errorMessages[error.code] || errorMessages.default,
+//     icon: "error",
+//     // confirmButtonColor: "#992127",
+//     ...config,
+//   });
+// }, 10);
+
+export const submitWithAlerts = async (submit, config = {}) => {
+  const { confirm: confirmConfig, success: onSuccessConfig, error: onErrorConfig } = config;
+  const { isConfirmed: shouldSubmit } = await confirm(confirmConfig);
+
+  if (!shouldSubmit) return null;
+
+  loading();
+  Swal.showLoading();
+
+  let response = null;
+  try {
+    response = await submit();
+    // throw new Error("Error");
+    // Swal.hideLoading();
+    onSuccess(onSuccessConfig);
+  } catch (error) {
+    // Swal.hideLoading();
+    onError(error, onErrorConfig);
+  } finally {
+    return response;
+  }
+};
+
+export default submitWithAlerts;
